53722b9d44ac47e2de53d510652b1229618bc5fd
Булат Хазиев <khazievbulatphanzilovich@gmail.com>
2021-03-25 23:21:58 +0300

    pattern realization

--- src/server_api.py
+++ src/server_api.py
@@ -1,6 +1,81 @@
 import json
-
+from abc import ABC, abstractmethod
+from requests.exceptions import RequestException
 import requests
+
+
+class ApiMethod(ABC):
+    def __init__(self, url, client):
+        self.url = url
+        self.client = client
+
+    @abstractmethod
+    def request(self):
+        def function(*args, **kwargs):
+            pass
+
+        return function
+
+
+class ApiGetMethod(ApiMethod):
+    def __init__(self, url, client):
+        super().__init__(url, client)
+        self.method = self.client.get
+
+    def request(self):
+        def function(*args, **kwargs):
+            try:
+                return self.method(
+                    url=self.url,
+                    headers=kwargs.get("headers") or {},
+                )
+            except RequestException or KeyError as e:
+                print(e)
+
+        return function
+
+
+class ApiPostMethod(ApiMethod):
+    def __init__(self, url, client):
+        super().__init__(url, client)
+        self.method = self.client.post
+
+    def request(self):
+        def function(*args, **kwargs):
+            try:
+                return self.method(
+                    url=self.url,
+                    headers=kwargs.get("headers") or {},
+                    data=kwargs.get("data") or {},
+                    files=kwargs.get("files") or {},
+                )
+            except RequestException or KeyError as e:
+                print(e)
+
+        return function
+
+
+class ApiFactory:
+    def __init__(self):
+        self.client = requests.session()
+        self.methods = {"GET": ApiGetMethod, "POST": ApiPostMethod}
+
+    def make(self, *args, **kwargs):
+        """Makes the ApiMethod that makes the request to the api
+        Args:
+            kwargs: method, url, headers, data, files
+        Returns:
+            function: pointer to a function
+        Raises:
+            RequestException, KeyError
+        """
+        try:
+            return self.methods[args[0]](url=args[1], client=self.client).request()
+        except KeyError as e:
+            print(e)
+
+    def update_headers(self, headers):
+        self.client.headers.update(headers)
 
 
 class ServerApi:
@@ -8,52 +83,41 @@ class ServerApi:
     csrftoken = ""
     token = ""
 
-    def __init__(self, url, auth_url):
-        self.url = url + "/api"
-        self.auth_url = auth_url
-        self.client = requests.session()
-        self.base_headers = {"accept": "application/json"}
+    def __init__(self, url):
+        self.factory = ApiFactory()
+        self.factory.update_headers({"accept": "application/json"})
+        self.api = {
+            "login": self.factory.make("POST", f"{url}/auth/sign_in/"),
+            "get_categories": self.factory.make("GET", f"{url}/category/"),
+            "get_category": lambda pk: self.factory.make("GET", f"{url}/category/{pk}/"),
+            "get_data": lambda pk: self.factory.make("GET", f"{url}/data/{pk}/"),
+            "create_data": self.factory.make("POST", f"{url}/data/"),
+        }
 
     def login(self, email, password):
-        credentials = {"email": email, "password": password}
-        response = self.client.post(
-            data=credentials, url=f"{self.auth_url}", headers=self.base_headers
-        )
+        response = self.api["login"](data={"email": email, "password": password})
         if response.status_code != 200:
             raise Exception("Неверный логин или пароль")
         self.token = json.loads(response.text)["auth_token"]
-        self.base_headers.update({"Authorization": f"Token {self.token}"})
+        self.factory.update_headers({"Authorization": f"Token {self.token}"})
 
     def get_categories(self):
-        response = self.client.get(f"{self.url}/category/", headers=self.base_headers)
-        return response
+        return self.api["get_categories"]()
 
     def get_category_data(self, category_id):
-        response = self.client.get(
-            f"{self.url}/category/{category_id}/", headers=self.base_headers
-        )
-        return response
+        return self.api["get_category"](category_id)()
 
-    def get_data(self, file_id):
-        response = self.client.get(
-            url=f"{self.url}/data/{file_id}/",
-            headers=self.base_headers,
-        )
-        return response
+    def get_data(self, data_id):
+        return self.api["get_data"](data_id)()
+
+    def create_data(self, data):
+        if isinstance(data["data"], str):
+            return self.api["create_data"](data=data)
+        else:
+            return self.api["create_data"](files=data)
 
     def create_file(self, file):
-        response = self.client.post(
-            url=f"{self.url}/file/",
-            headers=self.base_headers,
-            data={},
-            files=file,
-        )
-        return response
+        return self.factory.make("POST", "/file/")(files=file)
 
     def create_user_input(self, value):
-        response = self.client.post(
-            data={"data": value},
-            url=f"{self.url}/user_input/",
-            headers=self.base_headers,
-        )
-        return response
+        return self.factory.make("POST", "/user_input/")(data={"data": value})
